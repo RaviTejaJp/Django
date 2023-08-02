aa5c4b600a278b851596573739caccb259b62bd1
RT <ravitejajammulapati@gmail.com>
2023-08-02 08:20:19 +0530

    Removing hardcoded import to create modularisation

--- smartnotes/smartnotes/smartnotes/urls.py
+++ smartnotes/smartnotes/smartnotes/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-# Apps import
-from home import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', views.home)
+    path('', include('home.urls'))
 ]
